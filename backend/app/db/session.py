68042e259a7ef35251e2306ef7f152da70fcf748
Serhat-92 <yusuftumturk.921@gmail.com>
2025-12-23 10:59:36 +0300

    Veritabani baglantisi ayarlandi ve backend calisir hale getirildi

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # PostgreSQL connection string
 # TODO: Move to env variables via Pydantic settings
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/engelsiz_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/engelsiz_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
